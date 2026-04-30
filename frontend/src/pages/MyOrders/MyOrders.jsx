5fce30f985c2965c894c459a42a2af090e2ece9d
sps72057931 <sps72057931@gmail.com>
2026-04-30 11:34:32 +0530

    Updated currency symbol from $ to ₹ in MyOrders.jsx

--- frontend/src/pages/MyOrders/MyOrders.jsx
+++ frontend/src/pages/MyOrders/MyOrders.jsx
@@ -41,7 +41,7 @@ const MyOrders = () => {
                   }
                 })}
               </p>
-              <p>${order.amount}.00</p>
+              <p>₹{order.amount}.00</p>
               <p>items: {order.items.length}</p>
               <p>
                 <span>&#x25cf;</span>
