a0e7147708210edbb9f58663910909a10071c286
sps72057931 <sps72057931@gmail.com>
2026-04-30 13:31:32 +0530

    15th

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
 import { connectDB } from "./config/db.js";
 import foodRouter from "./routes/foodRoute.js";
 import userRouter from "./routes/userRoute.js";
@@ -7,20 +8,17 @@ import "dotenv/config";
 import cartRouter from "./routes/cartRoute.js";
 import orderRouter from "./routes/orderRoute.js";
 
-// app config
 const app = express();
 const port = process.env.PORT || 4000;
 
-// middlewares
 app.use(express.json());
 app.use(cors());
 
-// DB connection
 connectDB();
 
-// api endpoints
+app.use("/images", express.static(path.join(process.cwd(), "uploads")));
+
 app.use("/api/food", foodRouter);
-app.use("/images", express.static("uploads"));
 app.use("/api/user", userRouter);
 app.use("/api/cart", cartRouter);
 app.use("/api/order", orderRouter);
