d47f8377da28c2acf0989a0e4278704ede4f06bb
sps72057931 <sps72057931@gmail.com>
2026-04-30 14:42:48 +0530

    20

--- admin/src/components/Navbar/Navbar.jsx
+++ admin/src/components/Navbar/Navbar.jsx
@@ -1,98 +1,30 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import "./Navbar.css";
-import { assets } from "../../assets/frontend_assets/assets";
-import { Link, useNavigate } from "react-router-dom";
+import { assets } from "../../assets/assets";
 import { StoreContext } from "../../context/StoreContext";
 import { toast } from "react-toastify";
+import {useNavigate } from "react-router-dom";
 
-const Navbar = ({ setShowLogin }) => {
-  const [menu, setMenu] = useState("home");
-  const { getTotalCartAmount, token, setToken } = useContext(StoreContext);
-  const navigate = useNavigate();
-  const [darkMode, setDarkMode] = useState(false);
-
-  const toggleDarkMode = () => {
-    setDarkMode((prev) => !prev);
-    document.body.classList.toggle("dark-mode");
-  };
-
-  const logout = () => {
+const Navbar = () => {
+  const navigate=useNavigate();
+  const {token, admin, setAdmin, setToken } = useContext(StoreContext);
+  const logout=()=>{
     localStorage.removeItem("token");
+    localStorage.removeItem("admin");
     setToken("");
-    toast.success("Logout Successfully");
+    setAdmin(false);
+    toast.success("Logout Successfully")
     navigate("/");
-  };
-
+  }
   return (
     <div className="navbar">
-      <Link to="/">
-        <img src={assets.logo} alt="" className="logo" />
-      </Link>
-      <ul className="navbar-menu">
-        <Link
-          to="/"
-          onClick={() => setMenu("home")}
-          className={menu === "home" ? "active" : ""}
-        >
-          home
-        </Link>
-        
-          href="#explore-menu"
-          onClick={() => setMenu("menu")}
-          className={menu === "menu" ? "active" : ""}
-        >
-          menu
-        </a>
-        
-          href="#app-download"
-          onClick={() => setMenu("mobile-app")}
-          className={menu === "mobile-app" ? "active" : ""}
-        >
-          mobile-app
-        </a>
-        
-          href="#footer"
-          onClick={() => setMenu("contact-us")}
-          className={menu === "contact-us" ? "active" : ""}
-        >
-          contact us
-        </a>
-      </ul>
-      <div className="navbar-right">
-
-        {/* Dark Mode Toggle */}
-        <button className="dark-toggle" onClick={toggleDarkMode}>
-          {darkMode ? "☀️" : "🌙"}
-        </button>
-
-        <div className="navbar-search-icon">
-          <Link to="/cart">
-            <img src={assets.basket_icon} alt="" />
-          </Link>
-          <div className={getTotalCartAmount() === 0 ? "" : "dot"}></div>
-        </div>
-
-        {!token ? (
-          <button className="sign-in-btn" onClick={() => setShowLogin(true)}>
-            sign in
-          </button>
-        ) : (
-          <div className="navbar-profile">
-            <img src={assets.profile_icon} alt="" />
-            <ul className="nav-profile-dropdown">
-              <li onClick={() => navigate("/myorders")}>
-                <img src={assets.bag_icon} alt="" />
-                <p>Orders</p>
-              </li>
-              <hr />
-              <li onClick={logout}>
-                <img src={assets.logout_icon} alt="" />
-                <p>Logout</p>
-              </li>
-            </ul>
-          </div>
-        )}
-      </div>
+      <img className="logo" src={assets.logo} alt="" />
+      {token && admin ? (
+        <p className="login-conditon" onClick={logout}>Logout</p>
+      ) : (
+        <p className="login-conditon" onClick={()=>navigate("/")}>Login</p>
+      )}
+      <img className="profile" src={assets.profile_image} alt="" />
     </div>
   );
 };
