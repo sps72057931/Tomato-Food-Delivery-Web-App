c61e538dfbfe5cefb0d9d9fb21860b211346c1e3
sps72057931 <sps12311072@gmail.com>
2026-06-28 20:03:24 +0530

    26

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,11 @@
+import dns from "dns";
 import mongoose from "mongoose";
+
+// Change DNS
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
 export const connectDB = async () => {
   await mongoose
-    .connect(
-      process.env.MONGO_URL
-    )
-    .then(() =>console.log("DB Connected"));
+    .connect(process.env.MONGO_URL)
+    .then(() => console.log("DB Connected"));
 };
